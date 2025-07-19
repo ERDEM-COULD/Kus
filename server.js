bd8ece044799df5d5118aafa8f9c1ff12e867422
ERDEM-COULD <merdem58171.3@gmail.com>
2025-07-19 19:27:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
+  } else if (req.url === '/ping') {
+    // ✅ Uyanık kalmak için ping endpoint
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('pong');
   } else {
     res.writeHead(404);
     res.end();
